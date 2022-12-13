08e675b936d8f9571da389bd8f1d7a6f243161fe
Kajsia Anderson <kajsia.anderson@gmail.com>
2022-12-12 19:44:49 -0700

    initial commit. Example of authentication

---
+++ src/components/Auth.js
@@ -0,0 +1,59 @@
+import {useState, useContext} from 'react'
+import axios from 'axios'
+import AuthContext from '../store/authContext'
+ 
+const Auth = () => {
+    const authCtx = useContext(AuthContext)
+   const [username, setUsername] = useState('')
+   const [password, setPassword] = useState('')
+   const [register, setRegister] = useState(true)
+ 
+   const submitHandler = e => {
+       e.preventDefault()
+
+       const body = {
+        username,
+        password
+       }
+ 
+       axios.post(register ? "https://socialmtn.devmountain.com/register" : "https://socialmtn.devmountain.com/login", body)
+       .then((res) => {
+        console.log('data', res.data)
+        authCtx.login(res.data.token, res.data.exp, res.data.userId)
+       })
+       .catch(err => {
+        console.log('error', err)
+        setUsername('')
+        setPassword('')
+       })
+
+   }
+ 
+   return (
+       <main>
+           <h1>Welcome!</h1>
+           <form className='form auth-form' onSubmit={submitHandler}>
+               <input
+                   className='form-input'
+                   type='text'
+                   placeholder='username'
+                   onChange={e => setUsername(e.target.value)}
+                   value={username}
+                   />
+               <input
+                   className='form-input'
+                   type='password'
+                   placeholder='password'
+                   onChange={e => setPassword(e.target.value)}
+                   value={password}
+                   />
+               <button className='form-btn'>
+                   {register ? 'Sign Up' : 'Login'}
+               </button>
+           </form>
+           <button className='form-btn' onClick={() => setRegister(!register)}>Need to {register ? 'Login' : 'Sign Up'}?</button>
+       </main>
+   )
+}
+ 
+export default Auth
